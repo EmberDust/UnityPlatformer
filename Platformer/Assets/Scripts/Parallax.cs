71dab56c20f14933aab9e8c4858e666a0b3d6ffa
Ember Dust <emberdust162@gmail.com>
2021-10-07 21:28:00 +0500

    Add endless scrolling to parallax backgrounds, add global text as prefab

--- Platformer/Assets/Scripts/Parallax.cs
+++ Platformer/Assets/Scripts/Parallax.cs
@@ -4,26 +4,105 @@ public class Parallax : MonoBehaviour
 {
     [SerializeField] float _parallax = 0.5f;
     [SerializeField] float _verticalParallax = 0.1f;
+    [SerializeField] bool _scrolling = false;
+
+    [HideInInspector] public bool originalObject = true;
+    
+    GameObject _leftPart;
+    GameObject _centerPart;
+    GameObject _rightPart;
+
+    Vector2 _centerBGOrigin;
+
+    float _spriteWidth;
+    float _partsOffset;
 
     GameObject _mainCamera;
 
     Vector2 _cameraOrigin;
-    Vector2 _backgroundOrigin;
 
     void Start()
     {
-        _mainCamera = Camera.main.gameObject;
-        _cameraOrigin = _mainCamera.transform.position;
-        _backgroundOrigin = transform.position;
+        if (originalObject)
+        {
+            _mainCamera = Camera.main.gameObject;
+            _cameraOrigin = _mainCamera.transform.position;
+
+            _centerPart = this.gameObject;
+            _centerBGOrigin = _centerPart.transform.position;
+
+            if (_scrolling)
+            {
+                _spriteWidth = GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+                _partsOffset = _spriteWidth * transform.localScale.x;
+
+                _leftPart = Instantiate(this.gameObject, transform.parent);
+                _leftPart.GetComponent<Parallax>().originalObject = false;
+                _leftPart.transform.position = new Vector2(transform.position.x - _partsOffset, transform.position.y);
+
+                _rightPart = Instantiate(this.gameObject, transform.parent);
+                _rightPart.GetComponent<Parallax>().originalObject = false;
+                _rightPart.transform.position = new Vector2(transform.position.x + _partsOffset, transform.position.y);
+            }
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 
     void Update()
     {
-        Vector2 cameraPosition = _mainCamera.transform.position;
+        if (originalObject)
+        {
+            Vector2 cameraPosition = _mainCamera.transform.position;
 
-        Vector2 newBackgroundPosition = new Vector2 (_backgroundOrigin.x + (cameraPosition.x - _cameraOrigin.x) * _parallax,
-                                                     _backgroundOrigin.y + (cameraPosition.y - _cameraOrigin.y) * _verticalParallax);
+            if (_scrolling)
+            {
+                if (cameraPosition.x < _leftPart.transform.position.x)
+                {
+                    ScrollLeft();
+                }
+                else if (cameraPosition.x > _rightPart.transform.position.x)
+                {
+                    ScrollRight();
+                }
+            }
 
-        transform.position = newBackgroundPosition;
+            Vector2 newBackgroundPosition = new Vector2(_centerBGOrigin.x + (cameraPosition.x - _cameraOrigin.x) * _parallax,
+                                                         _centerBGOrigin.y + (cameraPosition.y - _cameraOrigin.y) * _verticalParallax);
+
+            _centerPart.transform.position = newBackgroundPosition;
+
+            if (_scrolling)
+            {
+                _leftPart.transform.position = newBackgroundPosition - new Vector2(_partsOffset, 0.0f);
+                _rightPart.transform.position = newBackgroundPosition + new Vector2(_partsOffset, 0.0f);
+            }
+        }
+    }
+
+    void ScrollLeft()
+    {
+        GameObject rightPart = _rightPart;
+
+        _rightPart = _centerPart;
+
+        _centerPart = _leftPart;
+        _centerBGOrigin -= new Vector2(_partsOffset, 0.0f);
+
+        _leftPart = rightPart;
+    }
+
+    void ScrollRight()
+    {
+        GameObject leftPart = _leftPart;
+
+        _leftPart = _centerPart;
+
+        _centerPart = _rightPart;
+        _centerBGOrigin += new Vector2(_partsOffset, 0.0f);
+
+        _rightPart = leftPart;
     }
 }
